84e2a73e734898268102f5d442356ac2846e7bad
mar-alv <mar.alv.cod@gmail.com>
2024-01-22 20:02:23 -0300

    feat: 24th project done

---
+++ 24/script.js
@@ -0,0 +1,17 @@
+const colorPreview = document.querySelector("#color-preview");
+const colorSlider = document.getElementById("input-color");
+
+function handleColorChange(event) {
+  const color = `hsl(${event.target.value}, 100%, 50%)`;
+
+  colorPreview.style.backgroundColor = color;
+  colorPreview.style.boxShadow = `0px 0px 53px 13px ${color}`;
+}
+
+function handleIlluminationChange(event) {
+  colorPreview.style.filter = `brightness(${event.target.value})`;
+}
+
+function handleContrastChange(event) {
+  colorPreview.style.filter = `contrast(${event.target.value})`;
+}
