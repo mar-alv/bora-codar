99920c2bcd2fe6ed2a9ffd12f13db305829c9663
mar-alv <mar.alv.cod@gmail.com>
2024-01-31 21:12:27 -0300

    feat: Most of project 28 style done

---
+++ 28/script.js
@@ -0,0 +1,6 @@
+const input = document.querySelector('input');
+const submitButton = document.querySelector('#submit-button');
+
+input.addEventListener('input', (event) => {
+  submitButton.disabled = !event.target.value;
+});
