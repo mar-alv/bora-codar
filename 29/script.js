d48cf4ec76f1c3732d0e074020af036fb6212305
mar-alv <mar.alv.cod@gmail.com>
2024-02-13 16:53:15 -0300

    feat: 29th project added

---
+++ 29/script.js
@@ -0,0 +1,8 @@
+const slider = document.querySelector("#slider");
+const sliderThumb = document.querySelector(".slider-thumb");
+const beforeImg = document.querySelector(".before-img-wrapper");
+
+slider.addEventListener("input", (e) => {
+  beforeImg.style.width = `${e.target.value}%`;
+  sliderThumb.style.left = `${e.target.value}%`;
+});
